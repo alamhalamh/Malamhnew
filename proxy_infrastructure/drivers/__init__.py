e5da6d855ae51940a1acb88155789c3a29793d6d
alamhalamh <mohamedalamh35@gmail.com>
2026-07-25 03:10:29 +0300

    Delete proxy_infrastructure/drivers directory

--- proxy_infrastructure/drivers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-__init__.py
-===========
-تصدير مدير البروكسي كطبقة موحدة جاهزة للاستخدام من بقية مكوّنات البوت.
-"""
-
-from .manager import proxy_manager
-from .tasks.health_checker import check_all_proxies_health
